ddb841c6e166cccb8d1c941f8c21f018c6aa61b4
yjyoon-dev <yjyoon.dev@gmail.com>
2023-03-22 00:46:49 +0900

    fix: User 모델 Serializable

--- app/src/main/java/dev/yjyoon/kwlibrarywearos/ui/model/User.kt
+++ app/src/main/java/dev/yjyoon/kwlibrarywearos/ui/model/User.kt
@@ -1,9 +1,11 @@
 package dev.yjyoon.kwlibrarywearos.ui.model
 
+import java.io.Serializable
+
 data class User(
     val id: String?,
     val password: String?,
     val phone: String?
-) {
+) : Serializable {
     val isValidate = id != null && password != null && phone != null
 }
