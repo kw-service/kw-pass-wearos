0dd8dc18e1bdbb7ba50dde5c1b0e3f52983397db
yjyoon-dev <yjyoon.dev@gmail.com>
2023-03-21 22:47:36 +0900

    feat: AccountInputType 정의 및 Chip 컴포넌트 리팩토링

--- app/src/main/java/dev/yjyoon/kwlibrarywearos/ui/account/AccountInputChip.kt
+++ app/src/main/java/dev/yjyoon/kwlibrarywearos/ui/account/AccountInputChip.kt
@@ -1,37 +1,52 @@
 package dev.yjyoon.kwlibrarywearos.ui.component
 
+import android.app.RemoteInput
+import android.content.Intent
+import android.view.inputmethod.EditorInfo
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.material.Chip
 import androidx.wear.compose.material.ChipDefaults
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.Text
+import androidx.wear.input.RemoteInputIntentHelper
+import androidx.wear.input.wearableExtender
 import dev.yjyoon.kwlibrarywearos.R
+import dev.yjyoon.kwlibrarywearos.ui.account.AccountInputType
 import dev.yjyoon.kwlibrarywearos.ui.theme.Primary
 import dev.yjyoon.kwlibrarywearos.ui.theme.White87
 
 @Composable
 fun KwLibraryChip(
     modifier: Modifier = Modifier,
-    icon: ImageVector,
-    label: String,
-    secondaryLabel: String?,
-    onClick: () -> Unit
+    type: AccountInputType,
+    input: String?,
+    onClick: (Intent) -> Unit
 ) {
-    val isValidate = secondaryLabel != null
+    val isValidate = input != null
+
+    val intent: Intent = RemoteInputIntentHelper.createActionRemoteInputIntent()
+    val remoteInputs: List<RemoteInput> = listOf(
+        RemoteInput.Builder(type.inputKey)
+            .setLabel(stringResource(type.stringResId))
+            .wearableExtender {
+                setInputActionType(EditorInfo.IME_ACTION_DONE)
+            }.build()
+    )
+
+    RemoteInputIntentHelper.putRemoteInputsExtra(intent, remoteInputs)
 
     Chip(
-        label = { Text(label) },
-        secondaryLabel = { Text(secondaryLabel ?: stringResource(id = R.string.unspecified)) },
-        onClick = onClick,
+        label = { Text(stringResource(id = type.stringResId)) },
+        secondaryLabel = { Text(input ?: stringResource(id = R.string.unspecified)) },
+        onClick = { onClick(intent) },
         icon = {
             Icon(
-                imageVector = icon,
+                imageVector = type.icon,
                 contentDescription = null
             )
         },
