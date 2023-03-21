83f97f352b7d060e8510d1c21ef51f885321597e
yjyoon-dev <yjyoon.dev@gmail.com>
2023-03-21 22:06:31 +0900

    chore: Color Pallette 수정

--- app/src/main/java/dev/yjyoon/kwlibrarywearos/ui/theme/Color.kt
+++ app/src/main/java/dev/yjyoon/kwlibrarywearos/ui/theme/Color.kt
@@ -3,16 +3,14 @@ package dev.yjyoon.kwlibrarywearos.ui.theme
 import androidx.compose.ui.graphics.Color
 import androidx.wear.compose.material.Colors
 
-val Primary = Color(0xFFFFDAD9)
-val PrimaryVariant = Color(0xFFA8353E)
-val Secondary = Color(0xFFFFDAD9)
-val SecondaryVariant = Color(0xFF775656)
+val Primary = Color(0xFFA8353E)
+val Secondary = Color(0xFF775656)
+val OnSecondary = Color(0xFFFFDAD9)
+val White87 = Color.White.copy(0.87f)
 
 internal val wearColorPalette: Colors = Colors(
     primary = Primary,
-    primaryVariant = PrimaryVariant,
     secondary = Secondary,
-    secondaryVariant = SecondaryVariant,
-    onPrimary = SecondaryVariant,
-    onSecondary = SecondaryVariant,
+    onPrimary = Color.White,
+    onSecondary = OnSecondary,
 )
