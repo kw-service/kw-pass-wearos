31ab76476e2c9b1caebc80d63845981b9a905e77
yjyoon-dev <yjyoon.dev@gmail.com>
2023-03-22 22:35:15 +0900

    feat: User 모델 자동 로그인 확인 필드 추가

--- app/src/main/java/dev/yjyoon/kwlibrarywearos/ui/model/User.kt
+++ app/src/main/java/dev/yjyoon/kwlibrarywearos/ui/model/User.kt
@@ -5,6 +5,7 @@ import java.io.Serializable
 data class User(
     val id: String,
     val password: String,
-    val phone: String
+    val phone: String,
+    val autoSignedIn: Boolean = false
 ) : Serializable
 
