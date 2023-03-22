bf986c5a7705a682f9f0a83280b4a3f33a350053
yjyoon-dev <yjyoon.dev@gmail.com>
2023-03-22 22:05:29 +0900

    fix: gson 의존성 제거

--- app/src/main/java/dev/yjyoon/kwlibrarywearos/data/di/DataModule.kt
+++ app/src/main/java/dev/yjyoon/kwlibrarywearos/data/di/DataModule.kt
@@ -18,7 +18,6 @@ import okhttp3.JavaNetCookieJar
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import java.net.CookieManager
 import javax.inject.Named
 import javax.inject.Singleton
@@ -55,7 +54,6 @@ internal object DataModule {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
             .client(okhttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
             .addConverterFactory(TikXmlConverterFactory.create(xmlParser))
             .build()
     }
