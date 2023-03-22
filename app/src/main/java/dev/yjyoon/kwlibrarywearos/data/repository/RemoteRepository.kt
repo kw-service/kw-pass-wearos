6012053566a98e680c7f1219650f5db272376b75
yjyoon-dev <yjyoon.dev@gmail.com>
2023-03-22 22:07:11 +0900

    feat: QR Code 생성 로직 구현

---
+++ app/src/main/java/dev/yjyoon/kwlibrarywearos/data/repository/RemoteRepository.kt
@@ -0,0 +1,30 @@
+package dev.yjyoon.kwlibrarywearos.data.repository
+
+import dev.yjyoon.kwlibrarywearos.data.source.ApiService
+import dev.yjyoon.kwlibrarywearos.data.util.EncryptionUtil.encode
+import dev.yjyoon.kwlibrarywearos.data.util.EncryptionUtil.encrypt
+import dev.yjyoon.kwlibrarywearos.ui.model.User
+import javax.inject.Inject
+
+class RemoteRepository @Inject constructor(
+    private val apiService: ApiService
+) {
+
+    suspend fun getQrCode(user: User): Result<String> = runCatching {
+        val rid = user.id.encode()
+        val realId = "0${user.id}".encode()
+        val secretKey = apiService.getSecretKey(userId = realId).item.secKey
+        val authKey = apiService.getAuthKey(
+            realId = realId,
+            rid = rid,
+            deviceGb = "A",
+            telNo = user.phone,
+            passWd = user.password.encrypt(secretKey)
+        ).item.authKey
+        apiService.getQrCode(
+            realId = realId,
+            authKey = authKey,
+            newCheck = "Y"
+        ).item.qrCode
+    }
+}
