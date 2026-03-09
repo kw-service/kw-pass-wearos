dc67c23c287348e87d350f464da3ace8c1bbb1d5
minmul <sksmstmdrud@gmail.com>
2026-03-09 19:44:04 +0900

    Update: GitHub 저장소 URI 변경(kw-service)

--- kwpass-phone/src/main/java/minmul/kwpass/service/KwPassUri.kt
+++ kwpass-phone/src/main/java/minmul/kwpass/service/KwPassUri.kt
@@ -1,6 +1,6 @@
 package minmul.kwpass.service
 
 object KwPassUri {
-    const val GITHUB_URI: String = "https://github.com/minmul07/kw-pass-wearos"
+    const val GITHUB_URI: String = "https://github.com/kw-service/kw-pass-wearos"
     const val STORE_URI: String = "https://play.google.com/store/apps/details?id=minmul.kwpass"
 }
